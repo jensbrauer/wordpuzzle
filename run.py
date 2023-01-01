d5b99a942dd06e98b3aa0c3e2633c9c0b153b9f3
jensbrauer <henningsson.jens@gmail.com>
2023-01-01 18:24:49 +0000

    Added responsemessage if scored point

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import puzzle
 
-
 from english_words import english_words_set
 
 
@@ -15,25 +14,35 @@ def make_strings(rows):
         string_list.append(''.join(col_string))
     return string_list
 
-def check_guess(word, board):
+def check_guess(word, board, scored_guesses):
     for string in board:
         if word in string and word in english_words_set:
-            return True
+            if word not in scored_guesses:
+                return True
     return False
 
 def run_game():
     puzzle_board = puzzle.get_puzzle()
     stringed_rows_and_cols = make_strings(puzzle_board[0])
     guess = ''
+    score = 0
+    scored_guesses = ['hej']
 
     while True:
         print('\n')
         for row in puzzle_board[0]:
             print('  '.join(row))
-        print(f'{guess}')
-        guess = input('Enter a word from the board:')
-        if check_guess(guess, stringed_rows_and_cols):
-            input('Nice, go again?')
+        print('\n')
+
+        if guess == scored_guesses[-1]:
+            print(f'"{guess}" scored you a point!')
+        else:
+            print(f'No points for "{guess}"! Go again!')
+        print(f'Current score: {score}')
+        guess = input('Enter a word (Q/q to quit):').lower()
+        if check_guess(guess, stringed_rows_and_cols, scored_guesses):
+            score += 1
+            scored_guesses.append(guess)
 
 
 
