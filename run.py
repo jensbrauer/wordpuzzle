66ca0f05ddfc74f606eb569ba4831036a5b869f3
jensbrauer <henningsson.jens@gmail.com>
2023-01-01 13:46:58 +0000

    Added run_game function

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import puzzle
 
-puzzle = puzzle.get_puzzle()
-print(puzzle[0])
-print(puzzle[1])
-print(puzzle[2])
+def run_game():
+    puzzle_board = puzzle.get_puzzle()
+    while True:
+        for row in puzzle_board[0]:
+            print('  '.join(row))
+        input('Enter a word from the board:')
+
+
+if input('Welcome to this wordpuzzle game, press enter to start!') == '':
+    run_game()
