1e86d94947c38dcdae460c304bca18e6e144d592
jensbrauer <henningsson.jens@gmail.com>
2023-01-02 04:12:21 +0000

    Compressed code

--- puzzle.py
+++ puzzle.py
@@ -1,62 +1,47 @@
 from english_words import english_words_set
 import random
 
-def is_vertical():
+def is_vert():
     if random.randint(0, 1) == 1:
         return True
     else:
         return False
 
 
-def get_position(word):
-    if is_vertical():
-        vertical_coordinate = [random.randint(0, 14 - len(word)), random.randint(0, 14)]
-        coordinates = []
+def get_pos(word):
+    if is_vert():
+        vert_coord = [random.randint(0, 14 - len(word)), random.randint(0, 14)]
+        coords = []
         for j in range(len(word)):
-            coordinates.append([vertical_coordinate[0] + j, vertical_coordinate[1]])
-        return coordinates
+            coords.append([vert_coord[0] + j, vert_coord[1]])
+        return coords
     else:
-        horizontal_coordinate = [random.randint(0, 14), random.randint(0, 14 - len(word))]
-        coordinates = []
+        horiz_coord = [random.randint(0, 14), random.randint(0, 14 - len(word))]
+        coords = []
         for j in range(len(word)):
-            coordinates.append([horizontal_coordinate[0], horizontal_coordinate[1] + j])
-        return coordinates
+            coords.append([horiz_coord[0], horiz_coord[1] + j])
+        return coords
 
 
 
 def get_puzzle():
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    screen = [
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
-    ]
+    screen = [['-' for x in range(15)] for y in range(15)]
     list_of_word = []
-    for word_to_print in random.sample(english_words_set, 500):
-        if len(word_to_print) < 14 and word_to_print[0].islower() :
-            start_position = get_position(word_to_print)
-            reference = ''
-            for i in word_to_print:
-                reference += '-'
-            appendix = []
-            for i in range(len(word_to_print)):
-                appendix.append(screen[start_position[i][0]][start_position[i][1]])
-            if ''.join(appendix) == reference:
-                for i in range(len(word_to_print)):
-                    screen[start_position[i][0]][start_position[i][1]] = word_to_print[i]
-                list_of_word.append(word_to_print)
+    for word in random.sample(english_words_set, 500):
+        if len(word) < 14 and word[0].islower():
+            if len(word) >= 2 and "'" not in word:
+                start_pos = get_pos(word)
+                reference = ''
+                for i in word:
+                    reference += '-'
+                appendix = []
+                for i in range(len(word)):
+                    appendix.append(screen[start_pos[i][0]][start_pos[i][1]])
+                if ''.join(appendix) == reference:
+                    for i in range(len(word)):
+                        screen[start_pos[i][0]][start_pos[i][1]] = word[i]
+                    list_of_word.append(word)
 
     for row in range(len(screen)):
         for space in range(len(screen[row])):
